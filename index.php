b5142621d77d63f8585298e274623e80c24b118b
Yusufia <Yusufiabichi@yahoo.com>
2025-02-19 11:21:06 -0800

    v

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ include ('include/config.php');
                             </div>
                         </div>
                         <div class="article-preview">
-                            <h2><?php echo $res['title'];?></h2>
+                            <h2><?php //echo $res['title'];?></h2>
                             <p class="price">&#8358;<?php echo $res['price']?></p>
                             <p class="card-desc">
                             <?php //echo $res['description'] ?>
